8df43bbd6281c5c4c48f4e13c34b55449766b4e6
罗远祥 <1141306760@qq.com>
2025-04-25 13:24:29 +0800

    feat: 更新项目配置并添加默认头像
    - 更新 .env 文件中的项目 API 地址
    - 在 Header、Footer 和 Author 组件中添加默认头像
    - 修改 layout.tsx 中的 favicon 逻辑
    - 在 project.d.ts 中添加 confetti 和 lantern 字段
    - 更新 request.ts 中的 fetch 请求，添加 credentials 配置
    -调整 CorsConfig 中的跨域配置

--- blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
+++ blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
@@ -9,9 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+//                .allowedOrigins("http://localhost:3000")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*") // 支持方法
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .maxAge(36000);
     }
 }
