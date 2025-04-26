67b127b092ea91b2f51e7d05b40abcab9c2b5aee
罗远祥 <1141306760@qq.com>
2025-04-26 08:09:21 +0800

    config: 更新跨域资源共享配置- 在 CorsConfig 类中添加了 allowedOrigins("*") 配置 - 此修改允许所有来源的跨域请求 - 提高了系统的可用性和灵活性，但也可能增加安全风险

--- blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
+++ blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
@@ -9,6 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*") // 支持方法
                 .allowedHeaders("*")
