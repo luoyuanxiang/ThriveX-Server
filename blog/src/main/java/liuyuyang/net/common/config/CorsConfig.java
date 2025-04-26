685d28e8cfa80b7d5c48847d295fa494679a1aea
罗远祥 <1141306760@qq.com>
2025-04-26 21:02:15 +0800

    refactor(blog): 更新 CorsConfig配置
    - 移除了 allowedOrigins("*") 配置- 添加了 allowedOriginPatterns("*") 配置

--- blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
+++ blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
@@ -9,7 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*") // 支持方法
                 .allowedHeaders("*")
