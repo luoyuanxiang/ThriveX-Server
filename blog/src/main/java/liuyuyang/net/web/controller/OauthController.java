99c0b1b69d63a1944d4ef48819ffe4390ea8997b
神秘人 <3311118881@qq.com>
2025-06-26 13:22:47 +0800

    集成 GitHub 登录授权 5

--- blog/src/main/java/liuyuyang/net/web/controller/OauthController.java
+++ blog/src/main/java/liuyuyang/net/web/controller/OauthController.java
@@ -1,6 +1,7 @@
 package liuyuyang.net.web.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import liuyuyang.net.common.utils.Result;
 import liuyuyang.net.web.service.OauthService;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +18,16 @@ public class OauthController {
     @Resource
     private OauthService oauthService;
 
-    @GetMapping("/github/{code}")
-    public Result githubLogin(@PathVariable String code) {
+    @ApiOperation("授权登录")
+    @PostMapping("/github/login")
+    public Result authGithubLogin(@RequestParam String code) {
+        Map<String, Object> result = oauthService.githubLogin(code);
+        return Result.success(result);
+    }
+
+    @ApiOperation("绑定第三方登录")
+    @GetMapping("/github/bind/{code}")
+    public Result bindGithubLogin(@PathVariable String code) {
         Map<String, Object> result = oauthService.githubLogin(code);
         return Result.success(result);
     }
