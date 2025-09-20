da34bfaea32f8fa0636bd8f023858631c098a092
罗远祥 <1141306760@qq.com>
2025-09-20 18:19:36 +0800

    feat(scheduling): 启用百度统计定时更新任务
    移除注释以启用百度统计的定时更新功能，确保数据能够定期同步。

--- src/main/java/top/luoyuanxiang/thrivex/server/ThrivexServerApplication.java
+++ src/main/java/top/luoyuanxiang/thrivex/server/ThrivexServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.reactive.function.client.WebClient;
 import top.luoyuanxiang.thrivex.server.entity.EnvConfigEntity;
 import top.luoyuanxiang.thrivex.server.service.IEnvConfigService;
@@ -42,7 +43,7 @@ public class ThrivexServerApplication {
      * 更新百度统计
      *
      */
-//    @Scheduled(fixedRate = 241920000)
+    @Scheduled(fixedRate = 241920000)
     public void updBaiduStatistics() throws Exception {
         EnvConfigEntity envConfig = envConfigService.getByName("baidu_statis");
         Map<String, Object> value = envConfig.getValue();
